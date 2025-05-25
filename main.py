4094b1626c6f25b6edfe79d46fa930135a41c31f
unknown <osama shaheen>
2025-05-25 15:07:15 +0200

    updates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from vector import retriever
+import sys
 
 model = OllamaLLM(model="llama3.2")
 
@@ -14,13 +15,13 @@ Here is the question to answer: {question}
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
-while True:
-    print("\n\n-------------------------------")
-    question = input("Ask your question (q to quit): ")
-    print("\n\n")
-    if question == "q":
-        break
+# while True:
+    # print("\n\n-------------------------------")
+question = sys.argv[1]
+    # print("\n\n")
+    # if question == "q":
+        # break
     
-    reviews = retriever.invoke(question)
-    result = chain.invoke({"reviews": reviews, "question": question})
-    print(result)
+reviews = retriever.invoke(question)
+result = chain.invoke({"reviews": reviews, "question": question})
+print(result)
